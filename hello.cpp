bb493549ae997577ad8a7683bea1aacd23fed3ee
abbas72O5 <abbas.raza722o5@gmail.com>
2025-09-15 10:58:22 +0500

    Add hello.cpp starter program

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to DSA Lab 1!" << endl;
+    cout<< "My name is Syed Abbas Raza"<<endl;
+    cout<< "My CMS ID is 517626"<<endl;
+    return 0;
+}
